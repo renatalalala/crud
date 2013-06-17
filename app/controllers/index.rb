1b15c22d08830d05cb9ff9b6968377d4a5fa0281
DBC-Apprentice <dbc@dbc.com>
2013-06-16 20:54:35 -0700

    fixed bug

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -17,12 +17,19 @@ get '/update/:id' do |note_id|
 	erb :update
 end
 
-post '/update/:id' do |note_id|
-	@note = Note.find(note_id)
-	@note.update_attributes(title: params[:title], content: params[:content])
-	@note.save
+put '/update/:id' do |note_id|
+	note = Note.find(note_id)
+	note.update_attributes(title: params[:title], content: params[:content])
+	note.save
 	redirect '/'
 end
 
-# delete '/note/:id' do
-# end
+get '/delete/:id' do |note_id|
+  @note = Note.find(note_id)
+  erb :delete
+end
+
+delete '/delete/:id' do |note_id|
+  note = Note.find(note_id).destroy
+  redirect '/'
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 NUM = 5
 
 NUM.times do 
-	Note.create(title: Faker::Lorem.words(5), content: Faker::Lorem.sentence(7))
+	Note.create(title: Faker::Lorem.sentence(1), content: Faker::Lorem.sentence(3))
 end
